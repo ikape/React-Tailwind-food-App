e993e1cac65a4fd0c64e11685fbaecca2506197c
ikape <ikapeisaac3@gmail.com>
2023-04-30 17:38:50 +0100

    fixed a classname bug

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,7 +4,7 @@ import { categories } from "../data/data";
 const category = () => {
   return (
     <div className='max-w-[1640px] m-auto px-4 py-12'>
-      <h1 text-orange-600 font-bold text-4xl text-center>
+      <h1 className='text-orange-600 font-bold text-4xl text-center'>
         Top Rated Menu Items
       </h1>
       {/* categories */}
