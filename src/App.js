bcb68e276b88d406b2a1cc0771ef7b72ded6fc50
ikape <ikapeisaac3@gmail.com>
2023-04-30 17:17:32 +0100

    run npm build

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Headlinecards from "./components/Headlinecards";
 import Food from "./components/Food";
-import Category from "./components/category";
+import Category from "./components/Category";
 
 function App() {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ import { FaUserFriends, FaWallet } from "react-icons/fa";
 import { MdFavorite, MdHelp } from "react-icons/md";
 
 const Navbar = () => {
-  const { nav, setNav } = useState(false);
+  const [nav, setNav] = useState(false);
 
   return (
     <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
@@ -22,7 +22,7 @@ const Navbar = () => {
         </div>
 
         <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>
-          Best <span className='font-bold'>Eats</span>
+          Just <span className='font-bold'>Eat</span>
         </h1>
 
         <div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'>
@@ -32,7 +32,7 @@ const Navbar = () => {
       </div>
 
       {/* Search inputs */}
-      <div className='bg-grey-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px] '>
+      <div className='bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px] '>
         <AiOutlineSearch size={25} />
         <input
           className='bg-transparent p-2 w-full focus:outline-none'
@@ -69,7 +69,7 @@ const Navbar = () => {
         />
 
         <h2 className='text-2xl p-4'>
-          Best <span className='font-bold'>Eats</span>
+          Just <span className='font-bold'>Eats</span>
         </h2>
 
         <nav>
